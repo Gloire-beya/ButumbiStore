8dfbd69c3b90ce827e8f2cae66528ab9bd140453
Gloire <gloweb78@gmail.com>
2021-07-25 09:00:28 +0200

    More roles have been added through a test method making the to six all together

--- ButumbiStoreWebParent/ButumbiStoreAdmin/src/test/java/com/butumbi/butumbistoreadmin/user/RoleRepositoryTest.java
+++ ButumbiStoreWebParent/ButumbiStoreAdmin/src/test/java/com/butumbi/butumbistoreadmin/user/RoleRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Commit;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
@@ -23,5 +25,17 @@ class RoleRepositoryTest {
         Role savedRole = roleRepository.save(admin);
 
         assertThat(savedRole.getName()).isEqualTo("Admin");
+    }
+    @Test
+    public void createRoles(){
+        Role salesPerson = new Role("Salesperson", "Manage sales report, manage customers, manage orders, update product price, view products, manage shipping rates");
+        Role customers = new Role("Customers","Post reviews, view products, view articles, vote reviews, vote questions, manage addresses, checkout, managing shopping cart, view orders");
+        Role editor = new Role("Editor", "Mange brands, manage products, mange menus, manage articles, manage categories");
+        Role assistant = new Role("Assistant", "Manage questions, manage reviews");
+        Role shipper = new Role("Shipper","View orders, update order status, view products");
+
+        List<Role> roles = (List<Role>)roleRepository.saveAll(List.of(salesPerson, customers, editor, assistant, shipper));
+
+        assertThat(roles.size()).isEqualTo(5);
     }
 }
